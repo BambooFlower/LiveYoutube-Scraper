23f54990799e0fce228aa3f7b800006121a35ea5
echomsk <72651265+echomsk@users.noreply.github.com>
2020-10-12 14:57:43 +0100

    release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BambooFlower/LiveYoutube-Scraper",
-    download_url =
+    download_url = 'https://github.com/BambooFlower/LiveYoutube-Scraper/archive/v1.0.0.tar.gz',
     keywords=["YouTube", "Live comments","scrape"],
     classifiers=[
         'Development Status :: 3 - Alpha',
